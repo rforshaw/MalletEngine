78f4a7ca3a1e8fd9331d91f9e3606feceb1e5f8a
Ross Forshaw <ross@QUEEG.(none)>
2013-07-26 13:30:44 +0100

    ConfigParser now allows you to add in custom game specific settings.

--- src/com/linxonline/mallet/io/reader/ConfigParser.java
+++ src/com/linxonline/mallet/io/reader/ConfigParser.java
@@ -1,65 +1,65 @@
 package com.linxonline.mallet.io.reader ;
 
+import java.util.ArrayList ;
+
 import com.linxonline.mallet.util.settings.* ;
 import com.linxonline.mallet.game.GameState ;
 
 public abstract class ConfigParser
 {
+	private static boolean initDefault = false ;
+	private final static ArrayList<ParseInterface> parsers = new ArrayList<ParseInterface>() ;
+
 	public ConfigParser() {}
+
+	public static void addParser( final ParseInterface _parse )
+	{
+		parsers.add( _parse ) ;
+	}
 
 	public static Settings parseSettings( final Settings _src, Settings _dest )
 	{
-		if( _src == null )
-		{
-			System.out.println( "Failed to Parse Config File." ) ;
-			return _dest ;
-		}
+		if( initDefault == false ) { init() ; }
+		if( _src == null ) { return _dest ; }
+		if( _dest == null ) { _dest = new Settings() ; }
 
-		if( _dest == null )
+		for( final ParseInterface parse : parsers )
 		{
-			_dest = new Settings() ;
-		}
-
-		String var = null ;
-		
-		try
-		{
-			var = _src.getString( "RENDERWIDTH" ) ;
-			_dest.addInteger( "RENDERWIDTH", Integer.parseInt( var ) ) ;
-		}
-		catch( Exception _ex ) {}
-
-		try
-		{
-			var = _src.getString( "RENDERHEIGHT" ) ;
-			_dest.addInteger( "RENDERHEIGHT", Integer.parseInt( var ) ) ;
-		}
-		catch( Exception _ex ) {}
-		
-		try
-		{
-			var = _src.getString( "DISPLAYWIDTH" ) ;
-			_dest.addInteger( "DISPLAYWIDTH", Integer.parseInt( var ) ) ;
-		}
-		catch( Exception _ex ) {}
-		
-		try
-		{
-			var = _src.getString( "DISPLAYHEIGHT" ) ;
-			_dest.addInteger( "DISPLAYHEIGHT", Integer.parseInt( var ) ) ;
-		}
-		catch( Exception _ex ) {}
-		
-		var = _src.getString( "MODE", "GAME_MODE" ) ;
-		if( var.equals( "GAME_MODE" ) == true )
-		{
-			_dest.addInteger( "MODE", GameState.GAME_MODE ) ;
-		}
-		else if( var.equals( "APPLICATION_MODE" ) == true )
-		{
-			_dest.addInteger( "MODE", GameState.APPLICATION_MODE ) ;
+			parse.parse( _src, _dest ) ;
 		}
 
 		return _dest ;
+	}
+
+	private static void init()
+	{
+		parsers.add( new ParseInterface()
+		{
+			public void parse( final Settings _src, final Settings _dest )
+			{
+				String var = null ;
+				var = _src.getString( "RENDERWIDTH", "640" ) ;
+				_dest.addInteger( "RENDERWIDTH", Integer.parseInt( var ) ) ;
+
+				var = _src.getString( "RENDERHEIGHT", "480" ) ;
+				_dest.addInteger( "RENDERHEIGHT", Integer.parseInt( var ) ) ;
+
+				var = _src.getString( "DISPLAYWIDTH", "640" ) ;
+				_dest.addInteger( "DISPLAYWIDTH", Integer.parseInt( var ) ) ;
+
+				var = _src.getString( "DISPLAYHEIGHT", "480" ) ;
+				_dest.addInteger( "DISPLAYHEIGHT", Integer.parseInt( var ) ) ;
+
+				var = _src.getString( "MODE", "GAME_MODE" ) ;
+				if( var.equals( "GAME_MODE" ) == true )
+				{
+					_dest.addInteger( "MODE", GameState.GAME_MODE ) ;
+				}
+				else if( var.equals( "APPLICATION_MODE" ) == true )
+				{
+					_dest.addInteger( "MODE", GameState.APPLICATION_MODE ) ;
+				}
+			}
+		} ) ;
 	}
 }
---
+++ src/com/linxonline/mallet/io/reader/ParseInterface.java
@@ -0,0 +1,8 @@
+package com.linxonline.mallet.io.reader ;
+
+import com.linxonline.mallet.util.settings.* ;
+
+public interface ParseInterface
+{
+	public void parse( final Settings _src, final Settings _dest ) ;
+}
